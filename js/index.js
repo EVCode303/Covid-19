6552b9ed03f329d0f61bd36aab74b2bab6e7fc98
Eduardo Varela <csmike81@gmail.com>
2020-06-23 13:52:34 -0600

    Se agregaron funciones para obtener info del dom y conseguir la informacion

--- js/index.js
+++ js/index.js
@@ -1,10 +1,43 @@
 'use strict';
 
+const covidInfo = "https://api.covid19api.com/summary";
+var cbxCountries;
+
 window.addEventListener("load", function(){
-	showPage();
+	start();
 });
 
-function showPage(){
-	document.querySelector("#carga").className += "fade";
+function start(){
+	showPage();
+	getDom();
+	fetchInfo();
 }
+
+function getDom(){
+	cbxCountries = document.querySelector("#countryPicker");
+}
+
+function fetchInfo(){
+	getCovidInfo()
+	.then(data => data.json())
+	.then(data => {
+		console.log(data);
+	})
+	.catch(e => {
+		console.log(e);
+	});
+}
+
+function fillCbx(){
+	
+}
+
+function showPage(){
+	document.querySelector("#carga").className += "fade";	
+}
+
+function getCovidInfo(){
+	return fetch(covidInfo);
+}
+
 
